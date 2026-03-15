cf9fc1240ce87855cab6e55164a97bdaecb22472
DinhThiLanHuong <23130131@st.hcmuaf.edu.vn>
2026-03-15 23:13:18 +0700

    them giao dien lien he

---
+++ src/main/java/cleanmeat/cleanmeat/controller/ Contact.java
@@ -0,0 +1,27 @@
+package cleanmeat.cleanmeat.controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "contact", value = "/contact")
+public class Contact extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setAttribute("pageTitle", "Liên hệ");
+        request.setAttribute("pageContent", "/view/contact.jsp");
+        request.setAttribute("pageCss", "contact.css");
+        request.setAttribute("active", "contact");
+
+        request.getRequestDispatcher("/view/base.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
