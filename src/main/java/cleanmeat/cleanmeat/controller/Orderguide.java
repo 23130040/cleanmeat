4af4ab9b150b61b41166a8c6ca379bc8e6847467
DinhThiLanHuong <23130131@st.hcmuaf.edu.vn>
2026-03-22 00:36:32 +0700

    Thêm hướng dẫn đặt hàng

---
+++ src/main/java/cleanmeat/cleanmeat/controller/Orderguide.java
@@ -0,0 +1,28 @@
+package cleanmeat.cleanmeat.controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "order-guide", value = "/order-guide")
+public class Orderguide extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.setAttribute("pageTitle", "Hướng dẫn đặt hàng");
+        request.setAttribute("pageContent", "/view/orderguide.jsp");
+        request.setAttribute("pageCss", "orderguide.css");
+
+        request.getRequestDispatcher("/view/base.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+}
